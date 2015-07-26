9d0e0858e05c95c2ea3f0eb429fbe2e14f8827f6
Gazotti <vtr.gomes@hotmail.com>
2015-07-26 13:23:38 -0300

    commit inicial slim

--- index.php
+++ index.php
@@ -1,6 +1,20 @@
 <?php 
 
-echo 'teste';
 
+require ('Slim/Slim/Slim.php');
+\Slim\Slim::registerAutoloader();
+$app = new \Slim\Slim();
+$app->response()->header('Content-Type', 'application/json;charset=utf-8');
+
+$app->get('/', function () {
+echo json_encode('Test');
+});
+
+$app->get('/test', function () {
+	echo json_encode('testinho');
+});
+
+
+$app->run();
 
 ?>
